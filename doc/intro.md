57288c4a3ee2f2f90484e212a11f0a5c05dd9631
Jim Turner <jturner314@gmail.com>
2012-08-17 21:04:49 -0400

    Add initial template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to solve-sudoku
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/solve_sudoku/core_test.clj
@@ -0,0 +1,7 @@
+(ns solve-sudoku.core-test
+  (:use clojure.test
+        solve-sudoku.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

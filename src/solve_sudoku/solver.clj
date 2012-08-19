fd6ee45c93283658d7fadbb44210a5ac33d900ce
Jim Turner <jturner314@gmail.com>
2012-08-19 13:50:42 -0400

    Add docstrings to the functions

--- src/solve_sudoku/solver.clj
+++ src/solve_sudoku/solver.clj
@@ -3,6 +3,8 @@
   (:require [clojure.string :as string]))
 
 (defn- valid-guesses
+  "Returns all the numbers from 1 through 9 that do not conflict with the row,
+  column, and 3x3 square corresponding to pos."
   [puzzle pos]
   (let [valid-nums (apply hash-set (range 1 10))
         [r c] pos
@@ -14,6 +16,7 @@
     (difference valid-nums row col square)))
 
 (defn- next-pos
+  "Returns next position in a sudoku puzzle, wrapping at the end of each row."
   [pos]
   (let [[r c] pos]
     (if (< c 8)
@@ -21,21 +24,29 @@
       [(inc r) 0])))
 
 (defn solve-puzzle
+  "Returns the solution to puzzle.  The puzzle should be a vector of vectors
+  with zeros representing the unknown cells. Optionally takes a starting
+  position; progresses rightward and downward, solving unknown cells."
   ([puzzle] (solve-puzzle puzzle [0 0]))
   ([puzzle pos]
      (cond
       (and (>= (pos 0) 8) (>= (pos 1) 8)) puzzle
       (not (zero? (get-in puzzle pos))) (recur puzzle (next-pos pos))
       :else (let [guesses (valid-guesses puzzle pos)
-                  solutions (map #(solve-puzzle (assoc-in puzzle pos %) (next-pos pos)) guesses)]
+                  solutions (map #(solve-puzzle (assoc-in puzzle pos %)
+                                                (next-pos pos))
+                                 guesses)]
               (some identity solutions)))))
 
 (defn puzzle->str
+  "Returns a string with a pretty representation of puzzle. Adds borders around
+  3x3 squares and replaces zeros with spaces."
   [puzzle]
   (let [col-sep \|
         row-sep "+---+---+---+\n"
         grouped (map (fn [row] (map #(apply str %) (partition 3 row))) puzzle)
-        rows (map (fn [row] (str col-sep (string/join col-sep row) col-sep \newline)) grouped)
+        rows (map (fn [row] (str col-sep (string/join col-sep row) col-sep \newline))
+                  grouped)
         rows-grouped (map #(string/join %) (partition 3 rows))
         stringified (str row-sep (string/join row-sep rows-grouped) row-sep)]
     (apply str (replace {\0 \space} stringified))))
